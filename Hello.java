aa4b5967e37548beead7d9fd56e47802f3edcf2c
jayshri487 <jayshri487@gmail.com>
2022-11-01 16:02:23 +0530

    first command

---
+++ Hello.java
@@ -0,0 +1,12 @@
+package test;
+
+public class Hello {
+	public static void main (String[] arg) {
+	System.out.println("Hello world");
+	System.out.println("I am learning Java");
+	System.out.println(3+3);
+	//Test program
+	
+	/* Test Program*/
+	}
+}
